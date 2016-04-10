76dcec43481c8ae628d6659d68d944a8376c2bc7
BalazsMarton <balazsmarton90.joomla@gmail.com>
2016-04-10 17:14:07 +0200

    chevron scroll

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function($){
 	// browser window scroll (in pixels) after which the "back to top" link is shown
 	var offset = 300,
 		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
+		offset_opacity = 3000,
 		//duration of the top scrolling animation (in ms)
 		scroll_top_duration = 700,
 		//grab the "back to top" link
